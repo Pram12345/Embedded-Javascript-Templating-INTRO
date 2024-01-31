358d2eaf79172346b658170065833f6e73924e1a
Gulshan kumar <121786265+Gulshanray@users.noreply.github.com>
2024-01-31 16:47:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    const usern = {
+        username: "ramakant"
+    };
+    res.render('index', { usern: usern });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
